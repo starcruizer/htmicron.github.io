a3e357d06c588293854fb7400b7e66ef415da11b
Hendrick Goncalves <hendrick.goncalves@htmicron.com.br>
2019-12-13 09:14:09 -0200

    Page updated.

--- MonarchScan/html/menudata.js
+++ MonarchScan/html/menudata.js
@@ -23,13 +23,6 @@ for the JavaScript code in this file
 var menudata={children:[
 {text:"Home",url:"../../index.html"},
 {text:"Main Page",url:"index.html"},
-{text:"Modules",url:"modules.html"},
-{text:"Data Structures",url:"annotated.html",children:[
-{text:"Data Structures",url:"annotated.html"},
-{text:"Data Structure Index",url:"classes.html"},
-{text:"Data Fields",url:"functions.html",children:[
-{text:"All",url:"functions.html"},
-{text:"Variables",url:"functions_vars.html"}]}]},
 {text:"Files",url:"files.html",children:[
 {text:"File List",url:"files.html"},
 {text:"Globals",url:"globals.html",children:[
